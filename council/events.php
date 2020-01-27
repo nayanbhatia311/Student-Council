2378f074a0e9e7d4ed83cb1242fc1de2c3fd2c43
Nayan <nayan.bhatia@somaiya.edu>
2020-01-27 20:34:45 +0530

    events and sponsors remaining

---
+++ council/events.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  
+  
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'><link rel="stylesheet" href="./header.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans|Oswald:300' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  
+  <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="./footer.css">
+</head> 
+<body>
+  
+  <?php include 'header.html';?>
+
+
+
+
+  
+<?php include 'footer.html';?>
+
+
+
+
+
+
+
+</body>
+
+</html>
+
+
+<script  src="./footer.js"></script>
