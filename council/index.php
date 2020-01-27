3ed60f7c99461f8d462f394ab1ac485a4156ce1b
Nayan <nayan.bhatia@somaiya.edu>
2020-01-27 18:22:31 +0530

    first commit

---
+++ council/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  
+  
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'><link rel="stylesheet" href="./header.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans|Oswald:300' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  
+  <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="./footer.css">
+</head> 
+<body>
+  
+  <?php include 'header.html';?>
+
+
+
+
+<!-- Main division -->
+  <div class="container">  
+          
+  <section id="features" class="padd-section text-center wow fadeInUp">
+
+<div class="container">
+
+<!-- Team Members div start -->
+  <div class="section-title text-center">
+    <h2>Team Members</h2>
+    <p class="separator">Koi acha sa cheesy line</p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>creative design</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>Retina Ready</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>easy to use</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>Free Updates</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>Free Updates</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>Free Updates</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>App store support</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>Perfect Pixel</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="feature-block">
+        <img src="img/" alt="img" class="img-fluid">
+        <h4>clean codes</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Team Members div end -->
+
+<!-- Video section -->
+<div class="container">
+  <div class="row">
+
+
+  </div>
+</div>
+
+
+<!-- video section ends -->
+
+
+
+
+</section>
+
+
+
+
+
+
+
+</div>
+<!-- Main division ends -->
+
+<?php include 'footer.html';?>
+
+
+
+
+
+
+
+</body>
+
+</html>
+
+
+<script  src="./footer.js"></script>
---
+++ flexbox-navigation-bar/flexbox-navigation-bar/README.markdown
@@ -0,0 +1,4 @@
+# Flexbox Navigation Bar
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/samnorton/pen/zBYMJG](https://codepen.io/samnorton/pen/zBYMJG).
+
+ 
