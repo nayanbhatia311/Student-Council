e7bca6b2550f1bc96dcd90a66debccd5d395615e
DharminChothani <dharminc24@gmail.com>
2020-02-02 02:55:12 +0530

    timeline integrated

--- council/timeline.php
+++ council/timeline.php
@@ -3,13 +3,25 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="./timeline.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+<link rel="stylesheet" href="./header.css">
 
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans|Oswald:300' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./footer.css">
 
 </head>
 <body>
 
+<?php include 'header.html'; ?>
+<br>
+
+
 <div class="timeline">
-  <div class="container left">
+  <div class="container-timeline left">
     <div class="content">
       <h2>STUDENT'S COUNCIL</h2>
       <img src='./images/about_us/studentcouncil.jpg' class="img-fluid" width="100%" height="30%">
@@ -20,14 +32,14 @@ The Students' Council of K. J. Somaiya Institute of Engineering and Information 
       
     </div>
   </div>
-  <div class="container right">
+  <div class="container-timeline right">
     <div class="content">
       <h2>SURGE</h2>
       <img src='./images/about_us/surge1.jpg' class="img-fluid" width="100%" height="30%">
       <p>SURGE, the annual cultural festival of KJSIEIT, with professional shows , events, and over 2000 people attending, has grown to be a never-to-miss opportunity in the cultural calendar of Mumbai. Over the years, SURGE has played host to great performances put up by people of immense talent. Inspired by the success of SURGE 2018, the Students' Council is filled with a gust of raw energy to make it bigger and finer. We believe that SURGE signifies a blend of various ideas, flavours and cultures.</p>
     </div>
   </div>
-  <div class="container left">
+  <div class="container-timeline left">
     <div class="content">
       <h2>SCORE</h2>
       <img src='./images/about_us/score.jpg' class="img-fluid" width="100%" height="30%">
@@ -35,25 +47,28 @@ The Students' Council of K. J. Somaiya Institute of Engineering and Information 
       <p>The S in Sport and Score stands for spirit and it's the greatest of all. Score is the annual sports festival of K. J. Somaiya Institute of Engineering and Information Technology. It has been a platform for multiple sports like cricket, table tennis, volleyball and many more. With every passing year, Score has seen an increase in the participation and tremendous efforts put in by them. Every time, the participants have proved that it really is about the spirit of playing. The Score team works together to make the current games better and introduce new ones which leads to more enthusiasts from other colleges as well. We hope to see Score grow every year and the players achieve their 'goals'.</p>
     </div>
   </div>
-  <div class="container right">
+  <div class="container-timeline right">
     <div class="content">
       <h2>Loren ipsum</h2>
       <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
     </div>
   </div>
-  <div class="container left">
+  <div class="container-timeline left">
     <div class="content">
       <h2>Loren ipsum</h2>
       <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
     </div>
   </div>
-  <div class="container right">
+  <div class="container-timeline right">
     <div class="content">
       <h2>Loren ipsum</h2>
       <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
     </div>
   </div>
 </div>
+<br>
+
+<?php include 'footer.html'; ?>
 
 </body>
 </html>
