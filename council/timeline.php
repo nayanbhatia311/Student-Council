9497664552a495040501b296e2642440f8a9aafc
Nayan <nayan.bhatia@somaiya.edu>
2020-02-02 02:01:35 +0530

    timeline.php added

---
+++ council/timeline.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./timeline.css">
+
+
+</head>
+<body>
+
+<div class="timeline">
+  <div class="container left">
+    <div class="content">
+      <h2>STUDENT'S COUNCIL</h2>
+      <img src='./images/about_us/studentcouncil.jpg' class="img-fluid" width="100%" height="30%">
+      <p>
+The Students' Council of K. J. Somaiya Institute of Engineering and Information Technology consists of students from every department of the college. They organise and execute various events throughout the year. They also are the mediator between faculty and the students, as they make sure that there is a continuous smooth relationship between the "two wheels" of the college. They host and arrange three festivals during the course of the year. In even semesters, they host Surge, the cultural festival and Score, the sports fest. In odd semesters, we have our technical fest, Renaissance. They strive to make all of the above festivals and events, memorable for every student and faculty. The Students' Council is of opinion that dreaming small is a crime. Every event organised during the academic year, should be the truest possible reflection of the essence regarding that event. They work together as a strong force to make Surge, the cultural fest of the college, Score, the sports fest of the college, and Renaissance, the technical fest of the college, the biggest celebration of every engineer's mind, talents and athletics; and every year they go closer to achieving that target.
+      </p>
+      
+      
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content">
+      <h2>SURGE</h2>
+      <img src='./images/about_us/surge1.jpg' class="img-fluid" width="100%" height="30%">
+      <p>SURGE, the annual cultural festival of KJSIEIT, with professional shows , events, and over 2000 people attending, has grown to be a never-to-miss opportunity in the cultural calendar of Mumbai. Over the years, SURGE has played host to great performances put up by people of immense talent. Inspired by the success of SURGE 2018, the Students' Council is filled with a gust of raw energy to make it bigger and finer. We believe that SURGE signifies a blend of various ideas, flavours and cultures.</p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="content">
+      <h2>SCORE</h2>
+      <img src='./images/about_us/score.jpg' class="img-fluid" width="100%" height="30%">
+      <b>"Do you know what my favorite part of the game is? The opportunity to play."- Mike Singletary. </b>
+      <p>The S in Sport and Score stands for spirit and it's the greatest of all. Score is the annual sports festival of K. J. Somaiya Institute of Engineering and Information Technology. It has been a platform for multiple sports like cricket, table tennis, volleyball and many more. With every passing year, Score has seen an increase in the participation and tremendous efforts put in by them. Every time, the participants have proved that it really is about the spirit of playing. The Score team works together to make the current games better and introduce new ones which leads to more enthusiasts from other colleges as well. We hope to see Score grow every year and the players achieve their 'goals'.</p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content">
+      <h2>Loren ipsum</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="content">
+      <h2>Loren ipsum</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content">
+      <h2>Loren ipsum</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
